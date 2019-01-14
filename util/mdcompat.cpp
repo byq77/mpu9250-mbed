351bcd996eaa8145d9d37a06f45a3bca3f9fac55
Szymon Szantula <byq77@hotmail.com>
2019-01-14 15:56:24 +0100

    mbed compatibility fixes

--- util/mdcompat.cpp
+++ util/mdcompat.cpp
@@ -1,6 +1,8 @@
 #include "mdcompat.h"
 #include <mbed.h>
  // must be out of the scope of extern "C"
+#define I2C_BUFFER 100
+static unsigned char buffer[I2C_BUFFER];
 
 I2C imu_i2c(MPU9250_I2C_SDA, MPU9250_I2C_SCL);
 Timer imu_timer;
@@ -24,29 +26,26 @@ int mbed_i2c_write(
     unsigned char length,
     unsigned char *data) {
     
-    unsigned char buffer[length+1];
     buffer[0]=reg_addr;
     for(int i=0;i<length;i++){
         buffer[i+1]=data[i];
     }
-    const char* x=(const char*)buffer;//
+    const char* x=(const char*)buffer;
     
     imu_i2c.write((int)slave_addr<<1,x,length+1,0);//sending register adress first to indicate which register we are writing and a write register(0)
     return 0;
 }
- 
+
 int mbed_i2c_read(
     unsigned char slave_addr,
     unsigned char reg_addr,
     unsigned char length,
-    unsigned char *data) {const char RA[]={reg_addr};
-    unsigned char buffer[length];
-    for(int i=0;i<length;i++){
-        buffer[i]=data[i];
-    }
-    imu_i2c.write((int)slave_addr<<1,RA, 1, 1); // no stop
-    imu_i2c.read((int)slave_addr<<1,(char*)data, length, 0); 
-    return 0; 
+    unsigned char *data)
+{
+    const char RA[] = {reg_addr};
+    imu_i2c.write((int)slave_addr << 1, RA, 1, 0);
+    imu_i2c.read((int)slave_addr << 1, (char *)data, length, 0);
+    return 0;
 }
 
 int delay_ms(
@@ -66,9 +65,6 @@ int reg_int_cb(
     unsigned char port,
     unsigned char pin)
 {
-    (void*)cb;
-    (void*)port;
-    (void*)pin;
     return 0;
 }
 
